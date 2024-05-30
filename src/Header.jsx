0df492adc88510792fceda0b134c1f0fe3eb1872
Your Name <you@example.com>
2024-05-30 14:30:07 +0700

    edit

---
+++ src/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <div className="logo">🚀 XYZ CORP</div>
+        <ul className="nav-list">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
